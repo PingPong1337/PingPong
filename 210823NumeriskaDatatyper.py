0d7eeb33f9caa1ea9de1336c3946b676aafb73cc
PingPong1337 <89184656+PingPong1337@users.noreply.github.com>
2021-08-23 15:08:53 +0200

    Add files via upload

---
+++ 210823NumeriskaDatatyper.py
@@ -0,0 +1,26 @@
+x = 5   		# heltal
+y = 3   		# ett till heltal
+print(x+y)  	# 8
+print(x-y)  	# 2
+print(x*y)  	# 15
+print(x/y)  	# 1.6666...
+print(x // y)   	# 1 (heltalsdivision)
+print(x % y)    	# 2 (restdivision)
+print(x**y) 	# 125, dvs. 5 upphöjt till 3
+print(-x)   	# -5
+print(+x)   	# 5
+print(abs(y-x))   # 2
+print(float(x)) 	# 5.0
+print(int(3.14))  # 3
+print(int("3")) 	# 3
+z = complex(1, 1) # komplext tal
+print(z)    	# (1+1j)
+print(z.conjugate())    # (1-1j)
+print(z*z)  	# (2j)
+
+e = 2,72
+
+π = 3.14
+
+print(2**z)
+print (z)
